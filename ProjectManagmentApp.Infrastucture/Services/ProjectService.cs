f685aa76c096166bf9c2f94005b4b2151f94cc1d
Aleksandar <extreeme92@hotmail.com>
2024-06-11 18:45:20 +0200

    updated project service method to save phase along with project.

--- ProjectManagmentApp.Infrastucture/Services/ProjectService.cs
+++ ProjectManagmentApp.Infrastucture/Services/ProjectService.cs
@@ -13,12 +13,14 @@ namespace ProjectManagmentApp.Infrastucture.Services
     public class ProjectService : IProjectService
     {
         private readonly IProjectRepository _projectRepository;
+        private readonly IPhaseRepository _phaseRepository;
         private readonly IMapper _mapper;
 
-        public ProjectService(IProjectRepository projectRepository, IMapper mapper)
+        public ProjectService(IProjectRepository projectRepository, IMapper mapper, IPhaseRepository phaseRepository)
         {
             _projectRepository = projectRepository;
             _mapper = mapper;
+            _phaseRepository = phaseRepository;
         }
 
         //***** CRUD METHODS *****//
@@ -55,6 +57,17 @@ namespace ProjectManagmentApp.Infrastucture.Services
         {
             var project = _mapper.Map<Project>(projectDTO);
             var createdProject = await _projectRepository.CreateAsync(project);
+
+            foreach (var phase in projectDTO.Phases)
+            {
+                await _phaseRepository.CreateAsync(new Phase
+                {
+                    Description = phase.Description,
+                    Name = phase.Name,
+                    ProjectId = createdProject.Id
+                });
+            }
+
             return _mapper.Map<ProjectDTO>(createdProject);
         }
 
@@ -64,6 +77,35 @@ namespace ProjectManagmentApp.Infrastucture.Services
             if (existingProject == null)
             {
                 return null;
+            }
+
+            foreach (var phase in existingProject.Phases.Where(x => x.Id != 0))
+            {
+                var phaseExist =
+                projectDTO.Phases
+                    .Select(x => x.Id)
+                    .Contains(phase.Id);
+
+                if (phaseExist)
+                {
+                    await _phaseRepository.UpdateAsync(phase);
+                    continue;
+                }
+                else
+                {
+                    await _phaseRepository.DeleteAsync(phase.Id);
+                }
+            }
+
+         
+            foreach (var phase in projectDTO.Phases.Where(x => x.Id == 0))
+            {
+                await _phaseRepository.CreateAsync(new Phase
+                {
+                    Description = phase.Description,
+                    Name = phase.Name,
+                    ProjectId = existingProject.Id
+                });
             }
 
             _mapper.Map(projectDTO, existingProject);
